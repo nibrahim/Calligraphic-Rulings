b3762acb4572dff7d60d8cb7ac07d60069ee4175
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-12-30 16:31:32 +0530

    Added code to draw angle lines

--- ruling.py
+++ ruling.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import math
 import optparse
 
 from reportlab.pdfgen import canvas, textobject, pdfimages
@@ -8,6 +9,16 @@ from reportlab.lib.pagesizes import A4
 
 __VERSION__ = "0.1alpha"
 
+# Utility functions
+def compute_endpoint(x0, y0, angle):
+    "Computes the endpoing for a given angle"
+    h = math.sqrt(sum(x**2 for x in A4))
+    x = x0 + h * math.cos(math.radians(angle))
+    y = y0 + h * math.sin(math.radians(angle))
+    return x, y
+    
+
+# Functions to draw different things on the canvas
 def draw_width_markers(canvas, nw):
     for i in range(0, int(300/nw)):
         if i%2 == 0:
@@ -40,6 +51,20 @@ def draw_ruling(canvas, nib_width, partitions, gap, nrulings, top_margin):
         position = (top_margin * mm * nib_width) + (i * line_height * nib_width * mm) # Margin + position for the current line
         offset = draw_single_line(canvas, position, nib_width, partitions)
         canvas.rect(1*mm, offset, A4[0], gap * nib_width * mm, stroke = 0, fill = 1)
+
+def draw_lines_for_angle(canvas, angle):
+    "Draws a few lines for the given angle"
+    for i in range(-200, 500, 20):
+        x0, y0 = 0, i*mm
+        x1, y1 = compute_endpoint(x0, y0, angle)
+        canvas.line(x0, y0, x1, y1)
+
+def draw_angle_lines(canvas, angles):
+    "Draws oblique lines to help with pen positioning and serifs"
+    print "Drawing angles"
+    angles = (float(x.strip()) for x in angles.split(","))
+    for i in angles:
+        draw_lines_for_angle(canvas, i)
         
 def write_title(canvas, text, partitions):
     canvas.setFillColorRGB(0.7,0.7,0.7)
@@ -53,6 +78,8 @@ def main(opts, args):
     c.setAuthor("ruling.py version %s"%__VERSION__)
     draw_width_markers(c, opts.nib_width)
     draw_ruling(c, opts.nib_width, opts.partitions, opts.gap, opts.rulings, opts.tmargin)
+    if opts.angles:
+        draw_angle_lines(c, opts.angles)
     if opts.title:
         write_title(c, opts.title, opts.partitions)
     c.showPage()
@@ -69,6 +96,8 @@ def parse_options(args):
                       help = "Top margin (specified in nib widths). Default is 2")
     parser.add_option("-r", "--rulings", dest = "rulings", default = 10, type=int,
                       help = "How many rulings to draw. Default is 10")
+    parser.add_option("-a", "--angle", dest = "angles", type = "string",
+                      help = "Comma separated list of angles (in degrees) for which to draw lines on the page (for pen angle, serifs etc.)")
     parser.add_option("-t", "--title", dest = "title", type = "string",
                       help = "A title for this ruling (usually the font name)")
 
