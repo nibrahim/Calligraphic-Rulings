6ef21fa9a76275ff9042c3b5819b7ef5841f1cff
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-27 16:54:36 +0530

    Added code to handle non / root URLs

--- calligraphic_rulings/web/app.py
+++ calligraphic_rulings/web/app.py
@@ -1,19 +1,24 @@
 import StringIO
 
 from ruling import main
+from .revproxy import ReverseProxied
 
 from flask import Flask, request, render_template, send_file, make_response
 
+APPLICATION_ROOT="/rulings"
+
 
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.config.from_object(__name__)
+app.config.debug = True
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/pdf")
+@app.route("/download")
 def pdf():
     cfg = lambda :0 # Dummy object which can have attributes
     for i,typ in [('nib_width', float),
