b06f1668d35124aea04096e78cb362fc9d231c0b
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-10-15 00:15:03 +0530

    Remove debugging output.

--- calligraphic_rulings/web/revproxy.py
+++ calligraphic_rulings/web/revproxy.py
@@ -19,7 +19,6 @@ class ReverseProxied(object):
         self.app = app
 
     def __call__(self, environ, start_response):
-        print "We're here!!!"
         script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
         if script_name:
             environ['SCRIPT_NAME'] = script_name
